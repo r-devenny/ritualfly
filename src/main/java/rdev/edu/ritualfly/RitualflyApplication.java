15fe1e5ff73435f2ddebbd62845e3d7207f8fa07
r-devenny <rdevenny9@gmail.com>
2025-09-07 16:18:58 +0100

    Initial commit

---
+++ src/main/java/rdev/edu/ritualfly/RitualflyApplication.java
@@ -0,0 +1,13 @@
+package rdev.edu.ritualfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitualflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RitualflyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rdev/edu/ritualfly/RitualflyApplicationTests.java
@@ -0,0 +1,13 @@
+package rdev.edu.ritualfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RitualflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
